fb9c46ec7bba8250ede839d48a6fd88fb36dbece
FERNANDES.CUNHA <lorena.ffc@outlook.com>
2020-07-16 18:58:00 -0300

    Project created

---
+++ UserRegistrationMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserRegistrationMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
